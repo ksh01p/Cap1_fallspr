3ef212467ec269ba197b1ed4e1105d025b14ef34
ksh01p <william7872ksh@handong.ac.kr>
2024-10-08 00:11:38 +0900

    DTO 까지 완성

---
+++ src/main/java/com/example/demo/domain/Board.java
@@ -0,0 +1,28 @@
+package com.example.demo.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Board {
+    @Id
+    Integer id;
+
+    String title;
+    String content;
+    String author;
+}
+
+/*
+DAO 의 종류
+
+Repo / Mapper 2가지가 존재함.
+Repo 는 자동_ Entity class 사용 , Mapper 는 수동
+PK 를 위한 annotation은 @Id ..
+
+*/
+
---
+++ src/main/java/com/example/demo/dto/NoticeDto.java
@@ -0,0 +1,47 @@
+package com.example.demo.dto;
+
+
+import com.example.demo.domain.Notice;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+public class NoticeDto {
+
+    @Setter
+    @Getter
+    public static class CreateReqDto {
+        private String title;
+        private String content;
+
+        public Notice toEntity(){
+            Notice notice = new Notice();
+            notice.setTitle(title);
+            notice.setContent(content);
+            return notice;
+        }
+    }
+    @Setter
+    @Getter
+    public static class UpdateReqDto {
+        private Long id;
+        private String title;
+        private String content;
+    }
+
+    //@Builder
+    @Setter
+    @Getter
+    public static class CreateResDto {
+        private Long id;
+    }
+
+    @Setter
+    @Getter
+    public static class DetailResDto {
+        private Long id;
+        private String title;
+        private String content;
+    }
+
+}
